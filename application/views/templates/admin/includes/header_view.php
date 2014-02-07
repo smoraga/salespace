79b67e715e739112d36d25b125860610969fe3df
aldrin15 <dioaldrin15@gmail.com>
2014-02-07 10:24:41 -0800

    Upload salespace html structure and login path

---
+++ application/views/templates/admin/includes/header_view.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Salespace - Admin</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url('assets/css/bootstrap.min.css')?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/bootstrap-reset.css')?>" rel="stylesheet">
+	
+    <!--external css-->
+    <link rel="stylesheet" href="<?php echo base_url('assets/font-awesome/css/font-awesome.css')?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/js/plugins/jquery-easy-pie-chart/jquery.easy-pie-chart.css')?>" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/owl.carousel.css')?>" />
+ 
+	<!-- Custom styles for this template -->
+    <link href="<?php echo base_url()?>assets/css/style.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/css/style-responsive.css" rel="stylesheet" />
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
