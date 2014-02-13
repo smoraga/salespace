23dc8477066a8d85eef67a524b7f6f06f9f0d565
Stephen Moraga <smoraga>
2014-02-13 10:26:43 +0800

    kenshin db config

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = '54.254.211.249';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'salespace';
+$db['default']['password'] = '12345';
+$db['default']['database'] = 'salespace_db';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
