64bb0798d0f30b2f189c474fec3d3f83fc38805b
Stephen Moraga <smoraga>
2014-02-13 16:40:18 +0800

    change type for supplier/reseller

--- application/views/templates/front_end/home/home_view.php
+++ application/views/templates/front_end/home/home_view.php
@@ -5,8 +5,8 @@
 	<title></title>
 </head>
 <body>
-<a href="javascript:void(0);" attr-name="supplier" onclick="set_client_type(1);">Register as Supplier</a>
-<a href="javascript:void(0);" attr-name="reseller" onclick="set_client_type(2);">Register as Reseller</a>
+<a href="javascript:void(0);" attr-name="supplier" onclick="set_client_type(2);">Register as Supplier</a>
+<a href="javascript:void(0);" attr-name="reseller" onclick="set_client_type(1);">Register as Reseller</a>
 <a href="javascript:void(0);" attr-name="buyer" onclick="set_client_type(3);">Register as Buyer</a>
 <h1>This is Home page</h1>
 <script type="text/javascript" src="assets/js/jquery-1.10.1.min.js"></script>
